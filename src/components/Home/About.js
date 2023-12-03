18f72b86993945de06372f0c02ac5eae442c257c
Moginder <mogiegan@gmail.com>
2023-12-03 15:58:41 +0530

    added home page

--- src/components/Home/About.js
+++ src/components/Home/About.js
@@ -6,11 +6,10 @@ const About = () => {
   const { home } = useContext(HomeContext);
 
   return (
-    <div className="block bg-[#3e9bc0] px-4 py-4 rounded-xl sm:flex sm:gap-10 mt-10 sm:mt-20">
-      <div className=" sm:w-1/4 w-4/5 mb-7 sm:mb-0 mx-auto">
-        <img src={homes} className="rounded-lg w-96  " alt="" />
-      </div>
-
+    <div className="block bg-[#3e9bc0] px-4 py-4 rounded-xl  sm:gap-10 mt-10 sm:mt-20">
+      <h1 className="sm:text-4xl text-xl font-mono py-2 text-[#a75959]">
+        OFFICE OF DIRECTOR (ACADEMIC RESEARCH){" "}
+      </h1>
       <div className="font-general-regular  mx-auto max-w-[75%] sm:w-3/4 text-justify">
         {home.map((bio) => (
           <p
